c927d420e3082f82d7258fd204dad3c3d8448346
kingkong1818 <hung810512@gmail.com>
2020-09-14 01:00:14 +0800

    Start backend manager

--- src/views/admin/Order.vue
+++ src/views/admin/Order.vue
@@ -2,6 +2,20 @@
   <div class="order">
     <h2>半吊子廚房訂單管理頁面</h2>
     <table class="table mt-3">
+      <thead>
+        <tr>
+          <th>分類</th>
+          <th>縮圖</th>
+          <th>名稱</th>
+          <th>原價</th>
+          <th>售價</th>
+          <th>啟用</th>
+          <th>編輯</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
     </table>
   </div>
 </template>
--- src/views/admin/Pic.vue
+++ src/views/admin/Pic.vue
@@ -3,7 +3,7 @@
     <div>
       <h1>這是後台上傳素材平台的啦！</h1>
       <input
-        id="customFile"
+        id="fileInput"
         type="file"
         class="form-control"
       >
@@ -12,13 +12,8 @@
       <table class="table mt-3">
         <thead>
           <tr>
-            <th>分類</th>
-            <th>縮圖</th>
-            <th>名稱</th>
-            <th>原價</th>
-            <th>售價</th>
-            <th>啟用</th>
-            <th>編輯</th>
+            <th>id</th>
+            <th>Path</th>
           </tr>
         </thead>
         <tbody>
@@ -34,7 +29,8 @@
 export default {
   data() {
     return {
-      filePath: '',
+      file: [],
+      // filePath: '',
       token: '',
     };
   },
@@ -42,7 +38,7 @@ export default {
     uploadFile() {
       // this.token = document.cookie.replace(/(?:(?:^|.*;\s*)
       // hexHomeworkToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-      const uploadedfile = document.querySelector('#customFile').file[0];
+      const uploadedfile = document.querySelector('#fileInput').file[0];
 
       const formData = new FormData();
       formData.append('file', uploadedfile);
